79a82fb036cc38b6facc217cbccb9ff2a6dc8626
Docile-Alligator <25734209+Docile-Alligator@users.noreply.github.com>
2024-10-09 16:32:14 -0400

    Use the custom Button component in Dialog.

--- components/Dialog.js
+++ components/Dialog.js
@@ -2,6 +2,7 @@ import React, {forwardRef} from 'react';
 import styled from 'styled-components';
 import Alert from "./Alert";
 import {useDispatch, useSelector} from "react-redux";
+import Button from "./Button";
 
 
 const Dialog = forwardRef(({className, message, content, onConfirm, onClose, onClearAlertMessage}, ref) => {
@@ -9,8 +10,8 @@ const Dialog = forwardRef(({className, message, content, onConfirm, onClose, onC
         <Container className={className} ref={ref}>
             <Alert message={message} onClose={onClearAlertMessage} />
             <div className="contentWrapper">{content}</div>
-            <button onClick={onClose}>Close</button>
-            <button onClick={onConfirm}>Confirm</button>
+            <Button text="Close" onClick={onClose} style={{display: "inline"}} size="medium" variant="neutral-light" />
+            <Button text="Confirm" onClick={onConfirm} style={{display: "inline"}} size="medium" variant="primary" />
         </Container>
     );
 });
