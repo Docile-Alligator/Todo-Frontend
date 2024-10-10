a5b432fce44b4eb39c8522ea82c22647bc5d94ca
Docile-Alligator <25734209+Docile-Alligator@users.noreply.github.com>
2024-10-10 17:20:56 -0400

    Add some comments in todos page.

--- pages/todos.js
+++ pages/todos.js
@@ -23,7 +23,7 @@ import Link from "next/link";
 const Todos = () => {
     const [activeTab, setActiveTab] = useState("incomplete");
 
-    // These two states can be used in the future to show a progress bar
+    // These two states can be used in the future to show a progress bar.
     const [incompleteLoading, setIncompleteLoading] = useState(true);
     const [allLoading, setAllLoading] = useState(true);
 
@@ -50,18 +50,29 @@ const Todos = () => {
     const [editTodoID, setEditTodoId] = useState("");
     const [editTodoName, setEditTodoName] = useState("");
 
-    // This state controls the 
+    // This state controls the confirm button active state in the dialog. Users will not be able to abuse the confirm button when performing the edit API call.
     const [isConfirmingEditingName, setIsConfirmingEditingName] = useState(false);
 
+    // Having this dialog ref allows us to perform show/hide operation of the dialog.
     const dialogRef = useRef(null);
 
     const dispatch = useDispatch();
+    /*
+        We are reusing this todo state used in the create page because they have similar data field.
+        The incomplete list and all list are passed through here.
+        Modifications to the todo item will be passed through here as well so that we can update the same item in both lists.
+    */
     const todoState = useSelector((state) => state.todo);
 
+    /*
+        The function for fetching the incomplete todo list.
+        It will show an alert message when something bad happens.
+     */
     const fetchIncompleteTodos = async ({before = undefined, after = undefined}) => {
         setIncompleteLoadingInfo("");
         setIncompleteLoading(true);
 
+        // Set the query params here so that it's easier to read and manage.
         const params = new URLSearchParams({
             before: before,
             after: after,
@@ -75,8 +86,13 @@ const Todos = () => {
             });
             const result = response.body;
             if (result.result.length === 0) {
+                // Notice we don't update the before and after anchor here since there is no more todo item available to fetch.
                 setIncompleteLoadingInfo("No data.");
             } else {
+                /*
+                    We dispatch a new state even if we don't need to update the all todo list since we have to make sure
+                    the same item should be updated in both lists after modification.
+                 */
                 dispatch(setIncompleteList({ incomplete: result.result }));
                 setIncompleteAnchor([result.before, result.after]);
             }
@@ -84,14 +100,20 @@ const Todos = () => {
             console.error(error);
             setIncompleteLoadingInfo("Server error.");
         } finally {
+            // Now we finished loading the incomplete todo list.
             setIncompleteLoading(false);
         }
     };
 
+    /*
+        The function for fetching the incomplete todo list.
+        It will show an alert message when something bad happens.
+     */
     const fetchAllTodos = async ({before = undefined, after = undefined}) => {
         setAllLoadingInfo("");
         setAllLoading(true);
 
+        // Set the query params here so that it's easier to read and manage.
         const params = new URLSearchParams({
             before: before,
             after: after,
@@ -104,9 +126,13 @@ const Todos = () => {
             });
             const result = response.body;
             if (result.result.length === 0) {
-                // No todos
+                // Notice we don't update the before and after anchor here since there is no more todo item available to fetch.
                 setAllLoadingInfo("No data.");
             } else {
+                /*
+                    We dispatch a new state even if we don't need to update the incomplete todo list since we have to make sure
+                    the same item should be updated in both lists after modification.
+                 */
                 dispatch(setAllList({ all: result.result }));
                 setAllAnchor([result.before, result.after]);
             }
@@ -114,18 +140,21 @@ const Todos = () => {
             console.error(error);
             setIncompleteLoadingInfo("Server error.");
         } finally {
+            // Now we finished loading the all todo list.
             setAllLoading(false);
         }
     };
 
+    // Load both lists when the page is rendering.
     useEffect(() => {
         fetchIncompleteTodos({});
-    }, []);
-
-    useEffect(() => {
         fetchAllTodos({});
     }, []);
 
+    /*
+        Clear the loading alert info for the previous tab when switch tabs.
+        Not clearing alert infos in both tabs at once is for user to see the initial loading info on a non-active tab
+     */
     useEffect(() => {
         if (activeTab === "incomplete") {
             setAllLoadingInfo("");
@@ -134,6 +163,10 @@ const Todos = () => {
         }
     }, [activeTab]);
 
+    /*
+        Return a promise when toggling the completeness so that the component itself can handle the rest.
+        This is because we want to show the alert message on the item in the todo list itself.
+     */
     const toggleTodoCompleteness = async (todoID, completed) => {
         return apiFetch("/todo/toggleCompleted", {
             method: "POST",
@@ -147,6 +180,7 @@ const Todos = () => {
     const showEditTodoDialog = (todoID, todoName) => {
         setIsConfirmingEditingName(false);
         if (dialogRef.current) {
+            // Set the corresponding state so when confirming the change, the onConfirmEditTodo will have the correct item to edit.
             setEditTodoId(todoID);
             setEditTodoName(todoName);
             dialogRef.current.showModal();
@@ -155,6 +189,7 @@ const Todos = () => {
 
     const onConfirmEditTodo = async () => {
         setIsConfirmingEditingName(true);
+        // Clear the message because we are doing something right now.
         dispatch(clearModifyTodoAlerts());
 
         const result = apiFetch("/todo/editName", {
@@ -169,6 +204,7 @@ const Todos = () => {
             if (result.status !== 200) {
                 dispatch(updateTodoNameError({ error: result.error }));
             } else {
+                // Edit name successfully and close the dialog.
                 if (dialogRef.current) {
                     dialogRef.current.close();
                 }
@@ -181,6 +217,10 @@ const Todos = () => {
         });
     }
 
+    /*
+        Return a promise when deleting the todo item so that the component itself can handle the rest.
+        This is because we want to show the alert message on the item in the todo list itself.
+     */
     const deleteTodo = async (todoID) => {
         return apiFetch("/todo/delete", {
             method: "DELETE",
